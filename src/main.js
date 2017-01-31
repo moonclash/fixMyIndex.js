1480e8d9e3a57aad803397732b2b78d94ccd2ac4
emil <emil.mladenov@dwellant.com>
2017-01-31 13:40:04 +0000

    added support for file input/output

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
-let str = 'h1 {z-index: 55;} h2 {z-index: 333;} .logo {z-index: 10}';
-let matches = str.match(regex);
-const regex = /z-index:\s\d{1,100}/gi;
+'use strict';
 
+const fs = require('fs');
+const regex = /z-index:\s\d{1,500}/gi;
 
 function splitAndConvert(val) {
   return +val.split(' ')[1];
 }
 
-matches = matches.sort((a,b) => splitAndConvert(a) > splitAndConvert(b));
+function fixMyIndex(input,output) {
+  fs.readFile(input, 'utf8', function(err, data) {
+        let result = data;
+        let matches  = result.match(regex);
+        matches = matches.sort((a,b) => splitAndConvert(a) > splitAndConvert(b));
+        matches.forEach((match,i) => {
+           result = result.replace(match,`z-index: ${i + 1}`);
+          });
+        fs.writeFile(output,result,'utf8')
+    });
+}
 
-matches.forEach((match,i) => {
-  str = str.replace(match,`z-index: ${i + 1}`);
-});
 
+fixMyIndex('screen.css','modded.css');
 //TODO - add support for negative z-index (should be easy, just modify the regex)
 //TODO - add support for strings with no space between z-index: and the numeric value
